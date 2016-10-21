a235494006c8472006effa76f69e7fd280cca130
Christian Samuel <christian.leumas@icloud.com>
2016-10-21 01:16:10 -0400

    added some documentation and refactored.

--- pxl_sorter.rb
+++ pxl_sorter.rb
@@ -6,27 +6,105 @@ require 'image_optim_pack'
 
 include Magick
 
+
+##
+# creates an instance variable with the name from the file/uri passed
+# example: name_parser('/Users/user/desktop/test.png') => @input_name = 'test'
+# only .png files can be passed
+def name_parser(uri)
+  base_uri = uri.dup
+  @input_name = File.basename(base_uri, '.png')
+end
+
+##
+# creates a uri by adding a directory path, a string and filename extension
+# example: new_path('/Users/user/desktop/', 'test') => '/Users/user/desktop/test.png'
+def new_path(path, name)
+  path + name + '.png'
+end
+
+##
+# creates an instance variable with the parent directory of the file path passed
+# example: path('/Users/user/desktop/test.png') => @path = '/Users/user/desktop/'
+# can only be run after the name_parser
+def parent_path(uri)
+  @parent_path = uri.dup
+  length = @input_name.length + 4
+  uri_length = @parent_path.length
+  start = uri_length - length
+  @parent_path[start..uri_length] = ''
+end
+
+def output_folder
+  @output_folder = "#{@parent_path}output/#{@input_name}/"
+end
+
+
+##
+# creates an instance variable with the parent directory of the output
+# example: output_folder => @sequence_folder = '/Users/user/desktop/output/test/sequence/'
+# can only be run after the name_parser and parent_path
+def sequence_folder
+  @sequence_folder = "#{@parent_path}output/#{@input_name}/sequence/"
+end
+
+##
+# creates an instance variable with the full path of the output
+# example: output_path => @@sequence_frame_path = '/Users/user/desktop/output/test/sequence/test.png'
+# can only be run after the name_parser and parent_path
+def sequence_frame_path
+  @sequence_frame_path = "#{@parent_path}output/#{@input_name}/sequence/#{@input_name}.png"
+end
+
+def image_folder
+  @image_folder = "#{@parent_path}output/#{@input_name}/#{@input_name}.png"
+end
+
+def make_dir?
+  FileUtils.mkdir_p(@sequence_folder) unless Dir.exists?(@sequence_folder)
+end
+
+def prepare(input)
+  name_parser(input)
+  parent_path(input)
+  sequence_folder
+  sequence_frame_path
+  image_folder
+  output_folder
+  make_dir?
+end
+
 def output(base_uri, input_name, output_name, options, gif, output_folder)
   base_uri = base_uri + 'output/'
-  Dir.mkdir(base_uri) unless Dir.exists?(base_uri)
+  if gif
+  else
+    Dir.mkdir(base_uri) unless Dir.exists?(base_uri)
+  end
+
   if output_name.nil?
     rando = '_' + (0...4).map{65.+(rand(26)).chr}.join.downcase
     settings_file = File.new(base_uri + input_name + rando + '.txt', "w")
     settings_file.puts(options.to_s)
     settings_file.close
     return base_uri +  input_name + rando + '.png'
+
+
   elsif gif || output_folder
     @base_uri = base_uri + "#{input_name}/"
-    Dir.mkdir(@base_uri) unless Dir.exists?(@base_uri)
-    settings_file = File.new(@base_uri + output_name + '.txt', "w")
+    # FileUtils.mkdir_p(@base_uri) unless Dir.exists?(@base_uri)
+    settings_file = File.new(@sequence_folder + output_name + '.txt', "w")
     settings_file.puts(options.to_s)
     settings_file.close
-    return @base_uri + output_name + '.png'
+    return @sequence_folder + output_name + '.png'
+
+
   elsif !gif && !output_folder
     settings_file = File.new(base_uri + output_name + '.txt', "w")
     settings_file.puts(options.to_s)
     settings_file.close
    return base_uri + output_name + '.png'
+
+
   end
 end
 
@@ -61,7 +139,7 @@ def uri_helper(location, file_name)
   if location == 'desktop'
     "/Users/#{ENV['USER']}/desktop/" + file_name + '.png'
   elsif location == 'downloads'
-    "/Users/#{ENV['USER']}/downloads/" + file_name + '.jpg'
+    "/Users/#{ENV['USER']}/downloads/" + file_name + '.png'
   end
 end
 
@@ -101,8 +179,9 @@ SEQUENCE_SETTINGS = { high_long: { counter: 1, max_multiple: 3, increment: 1, br
 
 ########### base uris ###########
 test = uri_helper('desktop', 'tes')
-testing = uri_helper('downloads', 'a')
-cinna = uri_helper('desktop', 'cinna')
+testing = uri_helper('downloads', 'test')
+
+
 ########## usable methods ##########
 
 
@@ -114,30 +193,42 @@ cinna = uri_helper('desktop', 'cinna')
 # example: glitch_sequence(test, SETTINGS[:side_glitch], 'test')
 # or
 # glitch_sequence(test, SETTINGS[:side_glitch], SEQUENCE_SETTINGS[:high_long],'test')
-def glitch_sequence(input, setting_hash, settings = SEQUENCE_SETTINGS[:high_short], output_name)
+def glitch_sequence(input, setting_hash, settings = SEQUENCE_SETTINGS[:high_short], compress = true, output_name)
   counter = settings[:counter]
+  prepare(input)
+
+  if counter > 1
+    path = @image_folder
+  else
+    path = @sequence_frame_path
+  end
+
+  if compress
+    compress(input, counter)
+  end
+  puts 'Begin glitch sequence.'
+
   image_number = 1
   while counter < settings[:break_point]
     setting_hash[:min] = counter
     setting_hash[:max] = counter * settings[:max_multiple]
-    brute_sort_save_with_settings(input, setting_hash, output_name + "_#{image_number}", true, true)
+    brute_sort_save_with_settings(path, setting_hash, output_name + "_#{image_number}", true, true)
     puts "FILE #{image_number} COMPLETE"
     image_number += 1
     counter += settings[:increment]
   end
-  FileUtils.cp(input, @base_uri) unless settings[:counter] > 1
-  gif(output_name)
+  gif(output_name, 84)
 end
 
 ##
 # at the moment, must be used with the glitch sequence method
-def gif(output_name)
-  animation = ImageList.new(*Dir["#{@base_uri}*.png"].sort_by { |x| x[/\d+/].to_i })
+def gif(output_name, speed)
+  animation = ImageList.new(*Dir["#{@sequence_folder}*.png"].sort_by { |x| x[/\d+/].to_i })
   animation.ticks_per_second=1000
   puts 'got images'
-  animation.delay = 84
+  animation.delay = speed
   puts 'creating gif'
-  animation.write("#{@base_uri}#{output_name}.gif")
+  animation.write("#{@output_folder}#{output_name}.gif")
   puts 'COMPLETE'
 end
 
@@ -148,16 +239,22 @@ def convert(img)
 end
 
 
-
-def compress(img)
-  image_optim = ImageOptim.new(allow_lossy: true, verbose: true, skip_missing_workers: true, optipng: false,
+def compress(img, counter = 1)
+  puts 'Compressing file.'
+  image_optim = ImageOptim.new(allow_lossy: true, verbose: false, skip_missing_workers: true, optipng: false,
                                 pngcrush: false, pngquant: {allow_lossy: true}, advpng: false, pngout: false, svgo: false)
-  FileUtils.cp(image_optim.optimize_image(img), '/Users/christiansamuel/desktop/output/')
+  if counter > 1
+    path = @image_folder
+  else
+    path = @sequence_frame_path
+  end
+  File.rename(image_optim.optimize_image(img), path)
+  puts 'File copied and compressed.'
 end
 
 ##
 # creates an image for each setting in the settings hash
-# quickest way to see how all of the settings effect the image supplied
+# quickest way to see how all of the settings affect the image supplied
 # an input uri(string) and an output_name(string) must be provided
 # the output name must only include the name of the output image not the file extension
 # the uri_helper can be used to create the input uri
@@ -165,26 +262,29 @@ end
 # barrage(test, 'test')
 # or
 # barrage("/Users/user/desktop/test.png", 'test')
-def barrage(input, output_name)
+def barrage(input, output_name, gif = false, speed = 84)
+  prepare(input)
+  puts 'Compressing file.'
+  compress(input)
   SETTINGS.each do |key, setting_hash|
-    brute_sort_save_with_settings(input, setting_hash, (output_name + "_#{key}"), false, true)
+    brute_sort_save_with_settings(input, setting_hash, (output_name + "_#{key}"), gif, true)
   end
+  gif(output_name, speed) unless !gif
 end
 
 
 ####################### use #######################
 
+# compress(test)
 
-
-convert(testing)
-
-# glitch_sequence(cinna, SETTINGS[:cinna], SEQUENCE_SETTINGS[:cinna],'cinna')
+# convert(testing)
+glitch_sequence(test, SETTINGS[:soft], SEQUENCE_SETTINGS[:high_short_early], 'blah')
 
 
 # 
-# barrage(test, 'test')
+# barrage(testing, 'test', true)
 
-# brute_sort_save_with_settings(test, SETTINGS[:soft])
+brute_sort_save_with_settings(test, SETTINGS[:soft], 'test')
 
 # Pxlsrt::Brute.brute("/Users/christiansamuel/desktop/test.png", :verbose=>true, :vertical=>true, :max=>80, 
 #                                                               :min=>10, :smooth=>true, :method=>'uniqueness', 
